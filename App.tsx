e1f4aebf7afc932d2719a30fddf2851e41f8ede1
Emanoel Augusto <emanoelaugusto7@gmail.com>
2024-09-12 12:43:53 -0300

    feat: create icon component and configuring svg

--- App.tsx
+++ App.tsx
@@ -4,14 +4,16 @@ import { Text } from './src/components/Text/Text';
 import { Button } from './src/components/Button/Button';
 import { ThemeProvider } from '@shopify/restyle';
 import { theme } from './src/theme/theme';
+import { EyeOnIcon } from './src/assets/icons/EyeOnIcon';
+import { Icon } from './src/components/Icon/Icon';
 
 function App(): React.JSX.Element {
     return (
         <ThemeProvider theme={theme}>
             <SafeAreaView>
                 <Text preset='headingLarge' bold italic>teste</Text>
-                <Button title='Primary'></Button>
-                <Button title='Outline' loading preset='outline'></Button>
+               
+                <Icon name='eyeOff' size={20} color='errorLight'/>
             </SafeAreaView>
         </ThemeProvider>
     );
