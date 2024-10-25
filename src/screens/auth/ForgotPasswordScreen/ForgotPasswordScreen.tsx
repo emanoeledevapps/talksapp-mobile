157df49e5150c05d1c22f5d7791ee801683325bd
Emanoel Augusto <emanoelaugusto7@gmail.com>
2024-10-25 11:30:41 -0300

    feat: add react hook form and schema validation

--- src/screens/auth/ForgotPasswordScreen/ForgotPasswordScreen.tsx
+++ src/screens/auth/ForgotPasswordScreen/ForgotPasswordScreen.tsx
@@ -6,11 +6,25 @@ import { Button } from "../../../components/Button/Button";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { RootStackParamList } from "../../../routes/Routes";
 import { useResetNavigationSuccess } from "../../../hooks/useResetNavigationSuccess";
+import {useForm} from 'react-hook-form';
+import { zodResolver } from "@hookform/resolvers/zod";
+import { forgotPasswordSchema, ForgotPasswordSchemaType } from "./forgotPasswordSchema";
+import { FormTextInput } from "../../../components/Form/FormTextInput";
 
 type ScreenProps = NativeStackScreenProps<RootStackParamList, 'ForgotPasswordScreen'>
 export function ForgotPasswordScreen({navigation}: ScreenProps){
     const {reset} = useResetNavigationSuccess();
-    function submitForm(){
+    const {control, formState, handleSubmit} = useForm<ForgotPasswordSchemaType>({
+        resolver: zodResolver(forgotPasswordSchema),
+        defaultValues: {
+            email: '',
+        },
+        mode: 'onChange',
+    });
+
+    function submitForm(formData: ForgotPasswordSchemaType){
+        console.log(formData);
+
         reset({
             title: 'Enviamos as instruções para seu e-mail',
             description: 'Clique no link enviado no seu e-mail para recuperar sua senha',
@@ -28,13 +42,20 @@ export function ForgotPasswordScreen({navigation}: ScreenProps){
             <Text preset="headingLarge">Esqueci minha senha</Text>
             <Text preset="paragraphLarge" mt='s24'>Digite seu e-mail e enviaremos as instruções para redefinição de senha</Text>
 
-            <TextInput
+            <FormTextInput
+                control={control}
+                name="email"
                 label="Email"
                 placeholder="Digite seu email"
                 boxProps={{mt: 's24'}}
             />
 
-            <Button title='Recuperar minha senha' mt='s24' onPress={submitForm}/>
+            <Button 
+                title='Recuperar minha senha' 
+                mt='s24' 
+                disabled={!formState.isValid}
+                onPress={handleSubmit(submitForm)}
+            />
         </Screen>
     )
 }
