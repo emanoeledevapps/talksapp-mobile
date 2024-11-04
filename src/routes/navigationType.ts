19abaca95eaf04a20e628eb80bdd5cb48008a21b
Emanoel Augusto <emanoelaugusto7@gmail.com>
2024-11-04 15:18:07 -0300

    test husky

--- src/routes/navigationType.ts
+++ src/routes/navigationType.ts
@@ -1,7 +1,15 @@
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
+
+import { AppStackParamList } from './AppStack';
 import { RootStackParamList } from './Routes';
 
 declare global {
-    namespace ReactNavigation {
-      interface RootParamList extends RootStackParamList {}
-    }
+  namespace ReactNavigation {
+    interface RootParamList extends RootStackParamList { }
   }
+}
+
+export type AppScreenProps<RouteName extends keyof AppStackParamList> = NativeStackScreenProps<
+  AppStackParamList,
+  RouteName
+>
--- src/screens/app/SettingsScreen/SettingsScreen.tsx
+++ src/screens/app/SettingsScreen/SettingsScreen.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import {Screen, Text } from '@components';
 
+const teste = ''
 export function SettingsScreen(){
     return(
         <Screen
