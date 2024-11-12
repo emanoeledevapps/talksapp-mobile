601cd0dbfab6a8daa348c0a437e5b728311de3b4
Emanoel Augusto <emanoelaugusto7@gmail.com>
2024-11-12 14:16:30 -0300

    feat: add screen tabs

---
+++ src/screens/app/NewPostScreen/NewPostScreen.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Screen, Text } from '@components';
+
+export function NewPostScreen(){
+    return(
+        <Screen>
+            <Text preset="paragraphLarge">New Post Screen</Text>
+        </Screen>
+    );
+}
