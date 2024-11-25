65c9b95ddb6a3cb018cc545ab87070dfb574b8e4
Emanoel Augusto <emanoelaugusto7@gmail.com>
2024-11-25 13:42:01 -0300

    feat: add shadow

--- src/routes/AppTabBar.tsx
+++ src/routes/AppTabBar.tsx
@@ -1,13 +1,30 @@
 import React from 'react';
+import { ViewStyle } from 'react-native';
 
 import { BottomTabBarProps } from '@react-navigation/bottom-tabs';
 
 import { Box, Icon, Text, TouchableOpacityBox } from '@components';
+import { useAppSafeArea } from '@hooks';
 import { AppTabBottomTabParamList, mapTapScreenToProps } from '@routes';
 
+const $shadowProps: ViewStyle = {
+    elevation: 10,
+    shadowColor: '#000',
+    shadowOpacity: 0.05,
+    shadowRadius: 12,
+    shadowOffset: {width: 0, height: -3}
+}
+
 export function AppTabBar({ state, navigation }: BottomTabBarProps) {
+    const {bottom} = useAppSafeArea();
+
     return (
-        <Box flexDirection="row">
+        <Box
+            flexDirection="row"
+            pt="s12"
+            style={[{paddingBottom: bottom}, $shadowProps]}
+            bg="background"
+        >
             {state.routes.map((route, index) => {
                 const tabData = mapTapScreenToProps[route.name as keyof AppTabBottomTabParamList];
 
@@ -21,29 +38,31 @@ export function AppTabBar({ state, navigation }: BottomTabBarProps) {
                     });
 
                     if (!isFocused && !event.defaultPrevented) {
-                    navigation.navigate(route.name, route.params);
+                        navigation.navigate(route.name, route.params);
                     }
                 };
 
                 const onLongPress = () => {
                     navigation.emit({
-                    type: 'tabLongPress',
-                    target: route.key,
+                        type: 'tabLongPress',
+                        target: route.key,
                     });
                 };
 
                 return (
                     <TouchableOpacityBox
+                        activeOpacity={1}
                         onPress={onPress}
                         onLongPress={onLongPress}
                         alignItems="center"
                         flex={1}
                     >
-                        <Icon 
-                            name={isFocused ? tabData?.icon.focused : tabData.icon.unfocused} 
+                        <Icon
+                            name={isFocused ? tabData?.icon.focused : tabData.icon.unfocused}
                             color={isFocused ? 'primary' : 'backgroundContrast'}
                         />
-                        <Text 
+                        <Text
+                            mt='s4'
                             preset="paragraphCaption"
                             color={isFocused ? 'primary' : 'backgroundContrast'}
                             semiBold
